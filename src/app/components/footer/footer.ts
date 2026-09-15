924e647e00f65084e6310834631e898d025f4f11
Gaurav-2803 <ranrovel1611@protonmail.com>
2026-09-15 22:30:20 +0530

    Fix: Hello World overriding over profile, blue underline on GA,

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import packageJson from '../../../../package.json';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +11,7 @@ import { CommonModule } from '@angular/common';
 })
 export class FooterComponent {
   year = new Date().getFullYear();
+  version = packageJson.version;
   navItems = ['About', 'Experience', 'Projects', 'Certifications', 'Contact'];
 
   scrollTo(section: string, event?: MouseEvent) {
